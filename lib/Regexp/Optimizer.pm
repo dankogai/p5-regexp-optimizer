b79b71328ad60e1c6b2f0485865da4f5a242a01c
Dan Kogai <dankogai+github@gmail.com>
2013-02-26 14:48:02 +0900

    VERSION 0.23

--- lib/Regexp/Optimizer.pm
+++ lib/Regexp/Optimizer.pm
@@ -4,7 +4,7 @@ use 5.008001;
 use strict;
 use warnings FATAL => 'all';
 use Regexp::Assemble;
-our $VERSION = sprintf "%d.%02d", q$Revision: 0.22 $ =~ /(\d+)/g;
+our $VERSION = sprintf "%d.%02d", q$Revision: 0.23 $ =~ /(\d+)/g;
 
 my $re_nested;
 $re_nested = qr{
@@ -88,7 +88,7 @@ Regexp::Optimizer - optimizes regular expressions
 
 =head1 VERSION
 
-$Id: Optimizer.pm,v 0.22 2013/02/26 04:51:15 dankogai Exp dankogai $
+$Id: Optimizer.pm,v 0.23 2013/02/26 05:47:41 dankogai Exp dankogai $
 
 =head1 SYNOPSIS
 
